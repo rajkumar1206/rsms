06476f2edf80e7cd7daab5a25f4e20fa0d773d44
rajkumar1206 <57241375+rajkumar1206@users.noreply.github.com>
2020-02-05 20:22:05 +0530

    Add files via upload

--- asssets/script/script.js
+++ asssets/script/script.js
@@ -1,14 +1,21 @@
-const parallax = document.querySelector('.image');
+// PARALLAX EFFECT CODE SPINNET
+const str = window.innerWidth;
+console.log(typeof str);
+if(str >= 800){
+        const parallax = document.querySelector('.image');
 
-parallax.style.backgroundPositionY = '0px';
+    parallax.style.backgroundPositionY = '0px';
 
-window.addEventListener('scroll',() => {
-    let offset = window.pageYOffset;
-    //console.log(parallax.style.backgroundPositionY);
-    parallax.style.backgroundPositionY = -offset*0.5 +'px';
-    //console.log(parallax.style.backgroundPositionY);
+    window.addEventListener('scroll',() => {
+        let offset = window.pageYOffset;
+        //console.log(parallax.style.backgroun dPositionY);
+        parallax.style.backgroundPositionY = -offset*0.5 +'px';
+        //console.log(parallax.style.backgroundPositionY);
 });
+}
 
+// TOGGLE ELEMENT CODE 
+const rotate = document.getElementsByClassName('rotate');
 const toggleElemenetList = Array.from( document.getElementsByClassName('innerblock') );
 //console.log(toggleElemenetList);
 
@@ -20,11 +27,38 @@ toggleElemenetList.forEach( element => {
     element.addEventListener('click', toggleHandler.bind(this));
 });
 
+
+//PRESET OF ANIMATION CODE
 const blockList = Array.from(document.querySelectorAll('.innerblock'));
 console.log(blockList);
 
 for( let i = 0 ; i< blockList.length ;i++){
     if(i%2==0){
         blockList[i].style.left = "-100%";
+    } else {
+        blockList[i].style.left = "100%";
     }
 }
+
+
+//ANIMATION EFFECT
+const id = document.getElementById('id');
+console.log(id);
+const animationHandler = document.querySelectorAll('.innerblock')[3];
+console.log(animationHandler.offsetTop);
+console.log(window.pageYOffset);
+
+window.addEventListener('scroll',() => {
+    if(id.offsetTop <= window.pageYOffset){
+        //console.log(window.pageYOffset)
+        for( let i = 0 ; i< blockList.length ;i++){
+                blockList[i].classList.add('animation');
+                setTimeout(()=>{
+                    blockList[i].style.left = '0%'
+                },1900)
+                
+        }
+    }
+    
+});
+
